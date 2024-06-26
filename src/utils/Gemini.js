18f72ba6b9aab386d9f4368be96b5c49cbd90549
Amit Maurya <139799731+amit-maurya0099@users.noreply.github.com>
2024-06-26 20:18:29 +0530

    firebase api key changed

--- src/utils/Gemini.js
+++ src/utils/Gemini.js
@@ -1,7 +1,7 @@
 import  { GoogleGenerativeAI } from "@google/generative-ai";
 
 // Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(process.env.REACT_APP_Gemini_KEY);
+const genAI = new GoogleGenerativeAI(process.env.REACT_APP_Firebase_API_KEY);
 
 const geminiModel = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 export default geminiModel;
