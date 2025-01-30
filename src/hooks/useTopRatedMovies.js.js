f7924e7b6fc6f5b3ceff0f5b104d5c63250b8b7d
Amit Maurya <139799731+amit-maurya0099@users.noreply.github.com>
2025-01-30 11:14:54 +0530

    changes in the apis

--- src/hooks/useTopRatedMovies.js.js
+++ src/hooks/useTopRatedMovies.js.js
@@ -8,7 +8,7 @@ const useTopRatedMovies=()=>{
     const dispatch=useDispatch();
     const topRatedMovies=useSelector((store)=>store.movies.topRatedMovies)
     const getTopRatedMovies=async()=>{
-        const data=await fetch('https://api.themoviedb.org/3/movie/top_rated?&pages=1',API_OPTIONS);
+        const data=await fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1',API_OPTIONS);
         const json=await data.json();
        
         dispatch(addTopRatedMovies(json.results))
